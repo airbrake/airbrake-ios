79fd33e1699e2012a65f89e0f1b4365888a4e8e3
Zach Drayer <zach@drayer.name>
2012-04-02 14:50:56 -0700

    Update KissXML, and use a static library target

--- Airbrake/kissxml/Categories/NSString+DDXML.m
+++ Airbrake/kissxml/Categories/NSString+DDXML.m
@@ -1,28 +1,30 @@
 #import "NSString+DDXML.h"
 
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
 
 @implementation NSString (DDXML)
 
-- (const xmlChar *)xmlChar
+- (const xmlChar *)ddxml_xmlChar
 {
 	return (const xmlChar *)[self UTF8String];
 }
 
 #ifdef GNUSTEP
-- (NSString *)stringByTrimming
+- (NSString *)ddxml_stringByTrimming
 {
 	return [self stringByTrimmingSpaces];
 }
 #else
-- (NSString *)stringByTrimming
+- (NSString *)ddxml_stringByTrimming
 {
 	NSMutableString *mStr = [self mutableCopy];
-	CFStringTrimWhitespace((CFMutableStringRef)mStr);
+	CFStringTrimWhitespace((__bridge CFMutableStringRef)mStr);
 	
 	NSString *result = [mStr copy];
 	
-	[mStr release];
-	return [result autorelease];
+	return result;
 }
 #endif
 
